cb587761106952f85cf087a554d6d387d1bb9888
dwep1337 <gemer942@gmail.com>
2025-02-19 14:12:16 -0300

    added: findByName Jpa

--- src/main/java/com/demo/tests/domain/PlanetRepository.java
+++ src/main/java/com/demo/tests/domain/PlanetRepository.java
@@ -3,6 +3,9 @@ package com.demo.tests.domain;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlanetRepository extends CrudRepository<Planet, Long> {
+    Optional<Planet> findByName(String name);
 }
